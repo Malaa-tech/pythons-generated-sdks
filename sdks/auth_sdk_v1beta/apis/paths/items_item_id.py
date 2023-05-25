b36e0bdd4a186c69afe64714c2cbded99f86b93b
abdulrahmanalotaibi <a.alotaibi.dev@gmail.com>
2023-05-25 19:49:42 +0300

    feat

--- sdks/auth_sdk_v1beta/apis/paths/items_item_id.py
+++
@@ -1,7 +0,0 @@
-from auth_sdk_v1beta.paths.items_item_id.get import ApiForget
-
-
-class ItemsItemId(
-    ApiForget,
-):
-    pass
--- sdks/auth_sdk_v1beta/paths/items_item_id/__init__.py
+++
@@ -1,7 +0,0 @@
-# do not import all endpoints into this module because that uses a lot of memory and stack frames
-# if you need the ability to import all endpoints from this module, import them with
-# from auth_sdk_v1beta.paths.items_item_id import Api
-
-from auth_sdk_v1beta.paths import PathValues
-
-path = PathValues.ITEMS_ITEM_ID
